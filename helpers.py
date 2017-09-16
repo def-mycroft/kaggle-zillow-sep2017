435be86216bd5b33a3a0d37e95a7d49d5eaab439
Joseph Dasenbrock <dasenbrock@gmail.com>
2017-09-16 14:41:03 -0500

    cut out helpers functions which I'm not going to use

--- helpers.py
+++ helpers.py
@@ -1,14 +1,9 @@
 """Helpers for general data analysis"""
 import pandas as pd
 import numpy as np
-from matplotlib import pyplot
-from tkinter import Tk
 from scipy.stats import ttest_ind as t_test
 import sqlite3
-db_path = "C:/Users/dasenbrock/OneDrive - CivicPlus/Chaotic/my_database.sqlite3"
-wfh_db_path = "C:/Users/dasen/Documents/my_database.sqlite3"
-wfh_out_path = "C:/Users/dasen/Documents/"
-wfh_downloads = "C:/Users/dasen/Downloads/"
+
 
 def inspect_database(filepath=db_path):
     """Returns list of tables in the database"""
@@ -32,10 +27,6 @@ def view_table_cols(table, filepath=db_path):
     cur = conn.execute('select * from %s' % table)
     return list(map(lambda x: x[0], cur.description))
 
-def create_url(internal_id):
-    """Creates a url given a client id"""
-    return r'https://system.netsuite.com/app/common/entity/custjob.nl?id=%s' % internal_id
-
 def nan_summary(df, columns, return_dict=False, display=True):
     """Given a df and list of cols, display missing value stats"""
     output = dict()
@@ -55,31 +46,6 @@ def nan_summary(df, columns, return_dict=False, display=True):
 
     if return_dict:
         return output
-
-def create_bar_charts(df, group_column, sum_column, filepath='', size=20):
-    """Create bar charts summing a value by a column"""
-    group_object = pd.DataFrame(df.groupby([group_column])[sum_column].sum())
-    group_object.plot(
-        kind='bar', figsize=(size,size*0.5))
-    # Graph title
-    graph_title = 'sum of %s grouped by %s' % (sum_column, group_column)
-    pyplot.suptitle(
-        graph_title, x=0.5, y=0.92, ha='center', fontsize='xx-large')
-
-    filepath += '%s-%s.png' % (sum_column, group_column)
-    pyplot.savefig(filepath)
-    pyplot.close()
-    filepath = filepath[:-4] + '.csv'
-    group_object.to_csv(filepath)
-
-def string_to_clipboard(string):
-    """Puts a string on the system clipboard"""
-    r = Tk()
-    r.withdraw()
-    r.clipboard_clear()
-    r.clipboard_append(string)
-    r.update()
-    r.destroy()
 
 def my_t_test(a, b, a_label='a', b_label='b'):
     """Given two array-like objects, calculates the P-Value"""
